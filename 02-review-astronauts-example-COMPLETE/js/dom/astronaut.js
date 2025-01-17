46c72772a2b01d226c5fe1d227badaa8e04d2b07
Daniel Mouris <dgmouris@gmail.com>
2025-01-17 09:29:55 -0700

    day 3 progress

--- 02-review-astronauts-example-COMPLETE/js/dom/astronaut.js
+++ 02-review-astronauts-example-COMPLETE/js/dom/astronaut.js
@@ -23,8 +23,8 @@ const renderAstronaut = (astronautData) => {
 
   return `<li href="#" class="list-group-item list-group-item-action">
       <div class="d-flex w-100 justify-content-between">
-          <img src="PROFILE THUMBNAIL HERE" class="rounded float-start" alt=""">
-          <h5 class="mb-1">ASTRONAUT NAME (ASTRONAUT STATUS HERE)</h5>
+          <img src="${astronautData.image.thumbnail_url}" class="rounded float-start" alt=""">
+          <h5 class="mb-1">${astronautData.name} (${astronautData.status.name})</h5>
           <small class="float-end">born DATE OF BIRTH HERE</small>
       </div>
       <small>ASTRONAUT NATIONALITY HERE (ASTRONAUT AGENCY NAME)</small>
