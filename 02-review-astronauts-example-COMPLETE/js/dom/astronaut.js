3203a2885c172b42569b843fdc7500cc7d6e23ef
Daniel Mouris <dgmouris@gmail.com>
2025-01-17 09:16:07 -0700

    day 3 progress

--- 02-review-astronauts-example-COMPLETE/js/dom/astronaut.js
+++ 02-review-astronauts-example-COMPLETE/js/dom/astronaut.js
@@ -17,3 +17,16 @@ replace the instances that have "THIS FORMAT HERE" with the astronautData.
 // return the html list item
 // export that function
 // use this function in the index.js
+const renderAstronaut = (astronautData) => {
+  return `<li href="#" class="list-group-item list-group-item-action">
+      <div class="d-flex w-100 justify-content-between">
+          <img src="PROFILE THUMBNAIL HERE" class="rounded float-start" alt=""">
+          <h5 class="mb-1">ASTRONAUT NAME (ASTRONAUT STATUS HERE)</h5>
+          <small class="float-end">born DATE OF BIRTH HERE</small>
+      </div>
+      <small>ASTRONAUT NATIONALITY HERE (ASTRONAUT AGENCY NAME)</small>
+      <p class="mb-1">ASTRONAUT BIO HERE </p>
+  </li>`
+}
+
+export {renderAstronaut}
