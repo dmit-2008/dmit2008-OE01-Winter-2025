332205e3dff3ae108668b7b45cbd72d87d4ab8c7
Daniel Mouris <dgmouris@gmail.com>
2025-01-24 09:34:50 -0700

    class progress pushed

---
+++ 04-05-nextjs-component-lists/components-lists-example/components/SimpsonsCharacters.js
@@ -0,0 +1,22 @@
+// copy the characters.
+const SIMPSON_CHARACTERS = [
+	"Homer Simpson",
+	"Bart Simpson",
+	"Marge Simpson",
+	"Mr. Burns",
+	"Lisa Simpson",
+	"Apu Nahasapeemapetilon",
+	"Sideshow Bob",
+	"Milhouse Van Houten",
+	"Ned Flanders",
+]
+
+// create a component
+// we're going to loop through all of the items
+export default function SimpsonsCharacters() {
+  // we're going to use our knowledge of using map
+  // to loop over the items
+  return <ul>
+
+  </ul>
+}
