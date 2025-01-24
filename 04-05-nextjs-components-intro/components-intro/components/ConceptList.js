5e5804e25b905e28f802edd8c35e0999786631b6
Daniel Mouris <dgmouris@gmail.com>
2025-01-24 09:06:48 -0700

    class progress pushed

---
+++ 04-05-nextjs-components-intro/components-intro/components/ConceptList.js
@@ -0,0 +1,9 @@
+// create a component that
+// wraps children with a ul
+// element
+
+// once done import and use in
+// index.js
+
+// Once done the above create a ConceptItem component.
+// that takes a single prop called idea
