23776f4f84d59047c3b4d30fa3ca36cec8e7e7ce
Daniel Mouris <dgmouris@gmail.com>
2025-01-24 09:41:24 -0700

    class progress pushed

--- 04-05-nextjs-component-lists/components-lists-example/pages/index.js
+++ 04-05-nextjs-component-lists/components-lists-example/pages/index.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+// import our list item
+import SimpsonsCharacters from "@/components/SimpsonsCharacters";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,24 +19,8 @@ export default function Home() {
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        {/* add our component here  */}
+        <SimpsonsCharacters />
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
