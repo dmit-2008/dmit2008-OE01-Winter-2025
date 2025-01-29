6ce73a2d92600c3c957a57e3098b319313f51862
Daniel Mouris <dgmouris@gmail.com>
2025-01-29 13:45:50 -0700

    class progress pushed

--- 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/index.js
+++ 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/index.js
@@ -1,3 +1,7 @@
+// taking a look at the documentation
+// I want you to add a container
+// add a row and column
+// add an h1 that says using react bootstrap.
 
 export default function Home() {
   return (
