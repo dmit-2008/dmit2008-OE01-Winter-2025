893e700cae31eec4025ba68f2f503f5c131b33de
Daniel Mouris <dgmouris@gmail.com>
2025-01-29 13:37:27 -0700

    class progress pushed

--- 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/_app.js
+++ 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/_app.js
@@ -1,5 +1,8 @@
 /*
+This is essentially the "entrypoint to the application."
 
+We're going to import our bootstrap entrypoint into this
+file. This is going to run on every single page.
 */
 
 import "@/styles/globals.css";
