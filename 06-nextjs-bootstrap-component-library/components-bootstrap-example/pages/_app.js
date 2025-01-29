dcb6e603f0128acfad279a644b5a4c4f7c0dcc6e
Daniel Mouris <dgmouris@gmail.com>
2025-01-29 13:40:31 -0700

    class progress pushed

--- 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/_app.js
+++ 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/_app.js
@@ -4,6 +4,7 @@ This is essentially the "entrypoint to the application."
 We're going to import our bootstrap entrypoint into this
 file. This is going to run on every single page.
 */
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import "@/styles/globals.css";
 
--- 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/index.js
+++ 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/index.js
@@ -11,7 +11,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function Home() {
+export default function Home({test}) {
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
