9ffd9e5e4bd6e2cdaafae7b59627289756a2b1dd
Daniel Mouris <dgmouris@gmail.com>
2025-01-31 09:17:19 -0700

    class progress pushed

--- 06-nextjs-mui-component-library/components-mui-example/components/Navbar.js
+++ 06-nextjs-mui-component-library/components-mui-example/components/Navbar.js
@@ -1,3 +1,4 @@
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -5,8 +6,17 @@ import Typography from '@mui/material/Typography';
 
 // we're creating the custom component that will contain the mui navbar
 export default function Navbar() {
-
-  return <>
-
-  </>
+  // take a look here https://mui.com/material-ui/react-app-bar/#basic-app-bar
+  // for the docs we're using some of the pieces here.
+  return <AppBar>
+    <Toolbar>
+      {/* I want you to look as an h6 but use a div to render */}
+      <Typography
+        variant="h6"
+        component="div"
+      >
+        Intro to MUI
+      </Typography>
+    </Toolbar>
+  </AppBar>
 }
