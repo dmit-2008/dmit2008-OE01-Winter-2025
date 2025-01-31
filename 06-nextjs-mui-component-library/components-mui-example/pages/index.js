211f08f8317c16ed6fc916cf3552dcfcb3d52d26
Daniel Mouris <dgmouris@gmail.com>
2025-01-31 09:02:55 -0700

    class progress pushed

--- 06-nextjs-mui-component-library/components-mui-example/pages/index.js
+++ 06-nextjs-mui-component-library/components-mui-example/pages/index.js
@@ -1,3 +1,7 @@
+// create a custom components called navbar
+// I want to you make the minimal version of a navbar using MUI.
+// I want you to use typography in the home component to display it.
+
 
 export default function Home() {
   return (
