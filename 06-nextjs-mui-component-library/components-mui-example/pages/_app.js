8767b811b7886d518e16f534294826306a7873b2
Daniel Mouris <dgmouris@gmail.com>
2025-02-05 13:50:08 -0700

    class progress pushed

--- 06-nextjs-mui-component-library/components-mui-example/pages/_app.js
+++ 06-nextjs-mui-component-library/components-mui-example/pages/_app.js
@@ -5,6 +5,18 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+// we're going to import the "themeProvider"
+import { ThemeProvider } from '@mui/material/styles';
+// we're going import our theme
+import { themeOptions } from "@/utils/theme/config";
+
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  // for my theme to be applied on the site completely
+  // I'm going to wrap my application component in the
+  // ThemeProvider and inject my theme.
+
+  return <ThemeProvider theme={themeOptions}>
+    <Component {...pageProps} />;
+  </ThemeProvider>
 }
