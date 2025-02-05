eca1545e46d0df2c094102a9bd5fae186038a9e8
Daniel Mouris <dgmouris@gmail.com>
2025-02-05 13:32:23 -0700

    class progress pushed

--- 06-nextjs-mui-component-library/components-mui-example/components/SimpleForm.js
+++ 06-nextjs-mui-component-library/components-mui-example/components/SimpleForm.js
@@ -2,12 +2,22 @@
 // create a component
 // has a form
 // and a text field
+import TextField from '@mui/material/TextField'
 // a check box with a label
 // a button
 // to use this in the index.js
 
 export default function SimpleForm() {
-  return <form>
+  // if I wanted to add padding on the form would it style or sx
+  // since form is not a "mui" element you need to use the style obj.
+  return <form
+    style={{marginTop: "10px"}}
+  >
+    <TextField
+      id="outlined-basic"
+      label="Our first text field"
+      variant="outlined"
+    />
 
   </form>
 }
