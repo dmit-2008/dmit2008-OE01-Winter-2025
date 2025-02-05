b9b52847ecbfc7d829c01446d80248eb0750ddc3
Daniel Mouris <dgmouris@gmail.com>
2025-02-05 13:38:23 -0700

    class progress pushed

--- 06-nextjs-mui-component-library/components-mui-example/components/SimpleForm.js
+++ 06-nextjs-mui-component-library/components-mui-example/components/SimpleForm.js
@@ -1,10 +1,14 @@
 // in this file I want you to
 // create a component
 // has a form
+// a button
+import Button from '@mui/material/Button';
+// a check box with a label
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 // and a text field
 import TextField from '@mui/material/TextField'
-// a check box with a label
-// a button
 // to use this in the index.js
 
 export default function SimpleForm() {
@@ -18,6 +22,15 @@ export default function SimpleForm() {
       label="Our first text field"
       variant="outlined"
     />
+    {/* So below you can see that you can
+    pass in jsx as a prop to a component */}
+    <FormGroup>
+      <FormControlLabel
+        control={<Checkbox defaultChecked />}
+        label="Check me"
+      />
+    </FormGroup>
 
+    <Button variant="contained">Submit this Form</Button>
   </form>
 }
