67a8fccc1209576decc19b9ea4a147e85bddce11
Daniel Mouris <dgmouris@gmail.com>
2025-02-14 08:44:07 -0700

    class progress pushed

--- 08-09-nextjs-event-state-fundamentals/state-events-fundamental-example/pages/index.js
+++ 08-09-nextjs-event-state-fundamentals/state-events-fundamental-example/pages/index.js
@@ -15,9 +15,12 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import TextField from '@mui/material/TextField';
 
+import DeleteIcon from '@mui/icons-material/Delete';
+
+// import my new list component
+import ListOfTodos from "@/components/ListOfTodos";
 import Navbar from "@/components/Navbar";
 
-import DeleteIcon from '@mui/icons-material/Delete';
 
 
 // make a form
@@ -135,51 +138,10 @@ export default function Home() {
             </Grid>
             {/* Create our list! (the full row) */}
             <Grid size={12}>
-              {/* make a list from mui */}
-              <List>
-                {/* we're going to change SAMPLE_TODOS
-                to our state value todoList */}
-                { todoList.map((todo, index)=> {
-                  // I need to return a single
-                  // jsx node.
-                  // note the console.log is for
-                  // learning purposes only and should be
-                  // removed when doing an assignment or code
-                  // (no console.logs in your jsx.)
-                  // console.log(`my todo here: ${todo}`)
-                  // I'm using the todo in the list
-                  // item because it exists.
-                  // let's create a delete button.
-                  // go take at mui see if you can
-                  // create this button.
-                  return <Fragment key={index}>
-                    {/*
-                      a fragment is like a "ghost" jsx node
-                      it's there so that you don't have to render a div
-                      most times fragments are written as <> jsx in here </>
-                    */}
-                    <ListItem
-                      secondaryAction={
-                        <IconButton
-                          edge="end"
-                          onClick={() => {deleteItemHandler(index)}}
-                        >
-                          {/* above i'm using a technique to pass in
-                          the existing index into a handler without
-                          directly calling it, because it's in a function */}
-                          <DeleteIcon />
-                        </IconButton>
-                      }
-                    >
-                      <ListItemText
-                        primary={todo}
-                        secondary={`index: ${index}`}
-                      />
-                    </ListItem>
-                    <Divider />
-                  </Fragment>
-                })}
-              </List>
+              <ListOfTodos
+                todoList={todoList}
+                deleteItemHandler={deleteItemHandler}
+              />
             </Grid>
           </Grid>
         </form>
