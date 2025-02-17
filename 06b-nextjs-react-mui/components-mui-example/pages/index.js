e7c5acbe6d0d4ba650e2197af409a4bce1bd37bd
oa <oliver+github@abolish.golf>
2025-02-16 17:53:00 -0700

    [feat] 06b - react-mui: Container, Box, and Typography implementation

--- 06b-nextjs-react-mui/components-mui-example/pages/index.js
+++ 06b-nextjs-react-mui/components-mui-example/pages/index.js
@@ -1,14 +1,21 @@
-import Head from "next/head";
-
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>MUI Example</title>
-      </Head>
         <main>
-          Is this text in Roboto?
+          <Container maxWidth="sm">
+            <Box sx={{ my: 4 }}>
+              <Typography variant="h2" component="h2">
+                MUI looks good.
+              </Typography>
+              <Typography variant="p" component="p">
+                You can perhaps see why this is a popular package.
+              </Typography>
+            </Box>
+          </Container>
         </main>
     </>
   );
