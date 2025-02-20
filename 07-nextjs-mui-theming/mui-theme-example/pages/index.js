0a727ead78cea8f0aa46302a56cc490de191ad54
oaao <oliver+github@abolish.golf>
2025-02-20 14:29:02 -0700

    [feat] 07 - react-mui-theming: load that font from a CDN so browsers can render in it

--- 07-nextjs-mui-theming/mui-theme-example/pages/index.js
+++ 07-nextjs-mui-theming/mui-theme-example/pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import styles from '../styles/Home.module.css'
 
 // material ui imports.
@@ -5,23 +6,29 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
+
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100&display=swap" rel="stylesheet" />
+      </Head>
+      <div className={styles.container}>
+        <main className={styles.main}>
 
-        <Container maxWidth="md">
-          <Box sx={{ my: 4 }}>
-            <Typography variant="h2" component="h2" color="primary">
-              MUI Example
-            </Typography>
-            <Typography variant="body1" component="p" color="secondary">
-              You can perhaps see why this is a popular package.
-            </Typography>
-          </Box>
-        </Container>
+          <Container maxWidth="md">
+            <Box sx={{ my: 4 }}>
+              <Typography variant="h2" component="h2" color="primary">
+                MUI Example
+              </Typography>
+              <Typography variant="body1" component="p" color="secondary">
+                You can perhaps see why this is a popular library.
+              </Typography>
+            </Box>
+          </Container>
 
-      </main>
-    </div>
+        </main>
+      </div>
   )
 }
