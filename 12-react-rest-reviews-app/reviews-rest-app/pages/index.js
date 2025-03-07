d4c2614f48397761c027fbc9af5929a063956f84
oaao <oliver+github@abolish.golf>
2025-03-06 18:50:05 -0700

    [feat] 12 - react-rest-reviews-app: - - - - initial next.js app state - - - -

---
+++ 12-react-rest-reviews-app/reviews-rest-app/pages/index.js
@@ -0,0 +1,38 @@
+import AppBar from '@mui/material/AppBar';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+
+import Container from '@mui/material/Container';
+
+
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Grid from '@mui/material/Grid';
+
+import IconButton from '@mui/material/IconButton';
+
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+
+import TextField from '@mui/material/TextField';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+export default function Home() {
+  return (
+    <>
+      <main>
+      </main>
+      </div>
+    </>
+  );
+}
