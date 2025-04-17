dc4ca05e3515a76245d6985e99d5bea3fe90bcf1
oaao <oliver+github@abolish.golf>
2025-04-17 17:48:13 -0600

    [feat] 22 - next-js-testing: TEST 1 - simple test to check title text

--- 22-next-js-testing-example/testing-application-example/test/TodoList.test.js
+++ 22-next-js-testing-example/testing-application-example/test/TodoList.test.js
@@ -8,7 +8,10 @@ test(
   "todo list title renders correctly",
   () => {
     // 1. set up the givens for the test (DOM, components, data, etc.)
+    render(<TodoList />)
     // 2. retrieve something we are testing via the DOM, e.g. a rendered component
+    const titleElement = screen.getByText("Our Todo List")
     // 3. declare what we expect to happen
+    expect(titleElement).toBeInTheDocument()
   }
 )
