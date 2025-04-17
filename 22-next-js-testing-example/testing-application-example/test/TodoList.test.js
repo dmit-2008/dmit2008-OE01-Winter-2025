f9d496d6e25ba26806ba4554d8853b605d5518c5
oaao <oliver+github@abolish.golf>
2025-04-17 17:37:24 -0600

    [feat] 22 - next-js-testing: minimum viable passing test

---
+++ 22-next-js-testing-example/testing-application-example/test/TodoList.test.js
@@ -0,0 +1,14 @@
+import { fireEvent, render, screen, act } from '@testing-library/react';
+import '@testing-library/jest-dom'
+
+import TodoList from '../components/TodoList'
+
+
+test(
+  "todo list title renders correctly",
+  () => {
+    // 1. set up the givens for the test (DOM, components, data, etc.)
+    // 2. retrieve something we are testing via the DOM, e.g. a rendered component
+    // 3. declare what we expect to happen
+  }
+)
